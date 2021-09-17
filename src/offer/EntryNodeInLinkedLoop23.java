1a8f8e703737413548ef15c22b052444015fda02
duktig666(RenShiWei) <1487660836@qq.com>
2021-09-17 18:06:23 +0800

    链表中环的入口节点

---
+++ src/offer/EntryNodeInLinkedLoop23.java
@@ -0,0 +1,65 @@
+package offer;
+
+/**
+ * description:剑指offer23题——链表中环的入口节点 @see https://leetcode-cn.com/problems/c32eOV/
+ *
+ * @author RenShiWei
+ * Date: 2021/9/17 17:26
+ **/
+public class EntryNodeInLinkedLoop23 {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) { this.val = val; }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 找到环的入口节点
+     */
+    public ListNode detectCycle(ListNode head) {
+        // 先找出环中的相遇节点
+        ListNode meetingNode = getMeetingNode(head);
+        // 没有环，返回null
+        if (meetingNode == null) {
+            return null;
+        }
+        // p1 指向快慢指针相遇的节点
+        ListNode p1 = meetingNode;
+        // p2 指向头节点
+        ListNode p2 = head;
+        // p1和p2以相同的速度向前移动，当p2指向环的入口节点时，p1已经围绕着环走了n圈又回到了入口节点。
+        while (p1 != p2) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        //返回入口节点
+        return p1;
+    }
+
+    /**
+     * 找到环中相遇的节点，不存在环返回null
+     */
+    private ListNode getMeetingNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                return slow;
+            }
+        }
+        return null;
+    }
+
+}
+
